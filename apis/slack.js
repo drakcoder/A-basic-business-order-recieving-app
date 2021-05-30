b37a24546e19ec0b292824760174c36e6544feb3
drakcoder <sairamgundala2000@gmail.com>
2021-05-30 20:14:05 +0530

    slack download api

--- apis/slack.js
+++ apis/slack.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const sgMail=require('@sendgrid/mail');
+const axios=require('axios');
 
 slackApiRoute=express.Router();
 
@@ -26,6 +27,25 @@ slackApiRoute.post('/send',(req,res)=>{
 slackApiRoute.get('/download',(req,res)=>{
     code=req.query.code;
     console.log(code);
+    const url="https://slack.com/api/oauth.access";
+    const params=new URLSearchParams();
+    params.append('client_id','2105721110720.2098371606609');
+    params.append('client_secret','aacb67f8122d0c5278d792835623fb88');
+    params.append('code',code);
+    params.append('redirect_uri','https://b5a683b1134b.ngrok.io/slack/download');
+    const config={
+        headers : {
+            'Content-Type':'application/x-www-form-urlencoded'
+        }
+    }
+    axios.post(url,params,config)
+        .then((result)=>{
+            console.log(result.data);
+        })
+        .catch((err)=>{
+            console.log('ERR')
+            console.log(err)
+        })
     res.send('sent')
 })
 
